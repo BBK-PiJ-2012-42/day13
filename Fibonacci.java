b7b4ca3efa0ae75ea346f44bb3dff1991b15c4e5
Tom Nealon <tgnealon@gmail.com>
2012-11-20 16:07:38 +0000

    Fibonacci commit.

--- Fibonacci.java
+++ Fibonacci.java
@@ -10,4 +10,38 @@ package day13;
  */
 public class Fibonacci {
     
+    public static int simpleFib(int n) {
+        if ((n == 1) || (n == 2)) {
+            return 1;
+        } else {
+        int result = simpleFib(n-1) + simpleFib(n-2);
+            return result;
+        }
+    }
+    
+    // arrays are 0-based, so F(1) is stored at position 0, etc
+    private int[] precalculated = null;
+    
+    public int memoFib(int n) {
+        if (precalculated == null) {
+            initPrecalculatedArray(n);
+        }
+        if (precalculated[n-1] != -1) {
+            return precalculated[n-1];
+        } else {
+            int result = memoFib(n-1) + memoFib(n-2);
+            precalculated[n-1] = result;
+            return result;
+        }
+    }
+    
+    private void initPrecalculatedArray(int size) {
+        precalculated = new int[size];
+        for (int i = 0; i < precalculated.length; i++) {
+            precalculated[i] = -1; // to indicate "not calculated yet"
+        }
+            precalculated[0] = 1; // F(1)
+            precalculated[1] = 1; // F(2)
+    }
+    
 }
---
+++ FibonacciTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day13;
+
+/**
+ *
+ * @author tom
+ */
+public class FibonacciTest {
+
+    
+    public static void main(String[] args) {
+        FibonacciTest test = new FibonacciTest();
+        test.launch();
+    }
+    
+    public void launch() {
+        Fibonacci myFib = new Fibonacci();
+        System.out.println("Simple 40:");
+        System.out.println(myFib.simpleFib(40));
+        System.out.println("Memoized 40:");
+        System.out.println(myFib.memoFib(40));
+        
+        System.out.println("Simple 45:");
+        System.out.println(myFib.simpleFib(45));
+        System.out.println("Memoized 45:");
+        System.out.println(myFib.memoFib(45));
+    }
+    
+}
